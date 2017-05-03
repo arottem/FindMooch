b04437ceadb1128231b30a3187ebd8390e36c930
amitair <new@rottem.net>
2017-05-03 14:47:56 -0700

    Icons for skill. Sequentially read out events (no location information sent, so default to Seattle)

---
+++ Alexa-Skill-Server/Alexa-Functions/Alexa-Response/MoochEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlexaFunctions
+{
+    public class MoochEvent
+    {
+        public string Id;
+        public string Description;
+        public string Title;
+        public string DetailsUri;
+        public DateTime EventStart;
+        public MoochLocation Location;
+
+    }
+}
--- Alexa-Skill-Server/Alexa-Functions/Alexa-Response/run.cs
+++ Alexa-Skill-Server/Alexa-Functions/Alexa-Response/run.cs
@@ -19,6 +19,28 @@ namespace AlexaFunctions
             // Get request body
             string data = await req.Content.ReadAsStringAsync();
             log.Info(data);
+            string outputSpeach = "Here are events near you.";
+
+            // Call Mooch API for data
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://findmooch.com");
+                var path = "/api/Events?token=9ML&latitude=0&longitude=0&hasLocation=0";
+                HttpResponseMessage moochResponse = await client.GetAsync(path);
+                if (moochResponse.IsSuccessStatusCode)
+                {
+                    //                    var rep = await moochResponse.Content.ReadAsStringAsync();
+                    var events = await moochResponse.Content.ReadAsAsync<IEnumerable<MoochEvent>>();
+                    foreach (MoochEvent moochEvent in events)
+                    {
+                        outputSpeach += String.Format("Event titled. {0}.", moochEvent.Title);
+                    }
+                }
+                else
+                {
+                    outputSpeach = "Sorry. An error occurred with Find Mooch.";
+                }
+            }
             var httpResponse = new
             {
                 version = "1.0",
@@ -28,7 +50,7 @@ namespace AlexaFunctions
                     outputSpeech = new
                     {
                         type = "PlainText",
-                        text = "How are we today?"
+                        text = outputSpeach
                     }
                 }
 
