61732bc9aed8c0bd103e66be5b915a825207e23f
amitair <new@rottem.net>
2017-05-04 21:22:54 -0700

    SSML readout of event title and date

--- Alexa-Skill-Server/Alexa-Functions/Alexa-Response/run.cs
+++ Alexa-Skill-Server/Alexa-Functions/Alexa-Response/run.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 namespace AlexaFunctions
 {
@@ -20,27 +21,33 @@ namespace AlexaFunctions
             string data = await req.Content.ReadAsStringAsync();
             log.Info(data);
             string outputSpeach = "Here are events near you.";
+            int pageSize = 5; // Get up to 5 events per interaction
+            string moochApiToken = ConfigurationManager.AppSettings["MoochApiToken"];
 
             // Call Mooch API for data
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri("http://findmooch.com");
-                var path = "/api/Events?token=9ML&latitude=0&longitude=0&hasLocation=0";
+                var path = String.Format("/api/Events?token={0}&latitude=0&longitude=0&hasLocation=0&pageSize={1}", moochApiToken, pageSize);
                 HttpResponseMessage moochResponse = await client.GetAsync(path);
                 if (moochResponse.IsSuccessStatusCode)
                 {
-                    //                    var rep = await moochResponse.Content.ReadAsStringAsync();
-                    var events = await moochResponse.Content.ReadAsAsync<IEnumerable<MoochEvent>>();
+                    var events = await moochResponse.Content.ReadAsAsync<ICollection<MoochEvent>>();
                     foreach (MoochEvent moochEvent in events)
                     {
-                        outputSpeach += String.Format("Event titled. {0}. ", moochEvent.Title);
+                        var localTime = TimeZoneInfo.ConvertTime(moochEvent.EventStart, TimeZoneInfo.Utc, TimeZoneInfo.Local);
+                        outputSpeach += String.Format(
+                            @"<p><emphasis level=""strong"">Event titled</emphasis> <break strength=""strong""/> {0} <break strength=""strong""/><emphasis level=""moderate""> Event occurs on </emphasis> {1} </p> ", 
+                            moochEvent.Title, localTime);
                     }
                 }
                 else
                 {
-                    outputSpeach = "Sorry. An error occurred with Find Mooch.";
+                    outputSpeach = @"<emphasis level=""strong"">Sorry.</emphasis> An error occurred with Find Mooch.";
                 }
             }
+
+            outputSpeach = String.Format("<speak>{0}</speak>", outputSpeach);
             var httpResponse = new
             {
                 version = "1.0",
@@ -49,7 +56,7 @@ namespace AlexaFunctions
                     shouldEndSession = "true",
                     outputSpeech = new
                     {
-                        type = "PlainText",
+                        type = "SSML",
                         text = outputSpeach
                     }
                 }
