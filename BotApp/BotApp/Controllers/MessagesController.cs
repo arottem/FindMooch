e82abe1da6f549267e2dda47d2bf71c1a9320c3e
amitair <new@rottem.net>
2017-06-04 00:23:44 -0700

    Mooch LUIS Bot first checkin

---
+++ BotApp/BotApp/Controllers/MessagesController.cs
@@ -0,0 +1,66 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Azure;
+using BotApp.Dialogs;
+using System;
+using System.Linq;
+
+namespace BotApp
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            // Initialize the azure bot
+            using (BotService.Initialize())
+            {
+                if (activity != null)
+                {
+                    // one of these will have an interface and process it
+                    switch (activity.GetActivityType())
+                    {
+                        case ActivityTypes.Message:
+                            await Conversation.SendAsync(activity, () => new BasicLuisDialog());
+                            break;
+                        case ActivityTypes.ConversationUpdate:
+                            var client = new ConnectorClient(new Uri(activity.ServiceUrl));
+                            IConversationUpdateActivity update = activity;
+                            if (update.MembersAdded.Any())
+                            {
+                                var reply = activity.CreateReply();
+                                var newMembers = update.MembersAdded?.Where(t => t.Id != activity.Recipient.Id);
+                                foreach (var newMember in newMembers)
+                                {
+                                    reply.Text = "Welcome";
+                                    if (!string.IsNullOrEmpty(newMember.Name))
+                                    {
+                                        reply.Text += $" {newMember.Name}";
+                                    }
+                                    reply.Text += "!";
+                                    await client.Conversations.ReplyToActivityAsync(reply);
+                                }
+                            }
+                            break;
+                        case ActivityTypes.ContactRelationUpdate:
+                        case ActivityTypes.Typing:
+                        case ActivityTypes.DeleteUserData:
+                        case ActivityTypes.Ping:
+                        default:
+                            //log.Error($"Unknown activity type ignored: {activity.GetActivityType()}");
+                            break;
+                    }
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+        }
+    }
+}
---
+++ BotApp/BotApp/Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Builder.Azure;
+using System.Configuration;
+
+namespace BotApp.Dialogs
+{
+
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"], ConfigurationManager.AppSettings["LuisAPIKey"])))
+        {
+        }
+
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"I'm sorry, I don't understand what you are asking. Ask me to find you events in your city");
+            context.Wait(MessageReceived);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "MyIntent" with the name of your newly created intent in the following handler
+        [LuisIntent("FindEvent")]
+        public async Task FindEventIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Sure, no problem - I'll find you some cool events in {result.Entities[0].Entity}"); 
+            context.Wait(MessageReceived);
+        }
+    }
+}
